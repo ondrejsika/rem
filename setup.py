165fa68eadd8af077fcefe92257cfbaa681bd112
Ondrej Sika <ondrej@ondrejsika.com>
2017-11-15 10:12:15 +0100

    setup.py - create python package

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rem',
+    version='1.0.0',
+    url = 'https://github.com/ondrejsika/rem',
+    license = 'MIT',
+    description = 'Simple repositories manager',
+    author = 'Ondrej Sika',
+    author_email = 'ondrej@ondrejsika.com',
+    scripts = [
+        'rem',
+    ],
+    install_requires = [],
+)
